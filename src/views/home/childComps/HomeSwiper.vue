43093b770876887c3a29da0fdbef921113f5d971
sarsimour <zhu.xiaoming36@gmail.com>
2020-04-01 13:19:36 +0800

    20200401初步完成主页

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners" v-bind:key="item.title">
+      <a :href="item.link">
+        <img :src="item.image" alt="" @load="homeswipperload">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+
+  export default {
+    name: "HomeSwiper",
+    data() {
+      return {
+        isload: false
+      }
+    },
+    props: {
+      banners: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    components: {
+      Swiper,
+      SwiperItem
+    },
+    methods: {
+      homeswipperload() {
+        if (! this.isload) {
+          this.$emit('swipperImageLoad');
+          this.isload = true;
+        }
+        
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
